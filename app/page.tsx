1469152d7fb2fce360236d56ba654fb8d434a592
PUSPANJALI PADHI <puspanjalipadhy444@gmail.com>
2025-12-12 18:23:12 +0530

    02 Database

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,6 @@ export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center h-screen">
       <Button>Hello World</Button>
-      <div>i am puspanjali</div>
     </div>
   );
 }
